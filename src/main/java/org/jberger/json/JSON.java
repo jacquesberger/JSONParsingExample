6ae28ed91b30a60c9138b931b15dbf0c3c33894a
Jacques Berger <jacquesberger@hotmail.com>
2012-04-20 22:39:07 -0400

    Migration du projet de SVN vers GitHub. Utilisation de Maven pour la gestion des dépendances.

---
+++ src/main/java/org/jberger/json/JSON.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2011 Jacques Berger.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jberger.json;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+public class JSON {
+
+    public static void main(String[] args) throws Exception {
+        String jsonTxt = FileReader.loadFileIntoString("json/library.json");
+
+        JSONArray root = (JSONArray) JSONSerializer.toJSON(jsonTxt);
+        int documentCount = root.size();
+        for (int i = 0; i < documentCount; i++) {
+            JSONObject document = root.getJSONObject(i);
+            if (document.getString("type").equals("book"))
+                System.out.println(document.getString("title") + " publié en " + document.getInt("year"));
+        }
+    }
+}
