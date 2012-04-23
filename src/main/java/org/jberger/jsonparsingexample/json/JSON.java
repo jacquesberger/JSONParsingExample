a523f874e192facb78a71613c4deee53217c6c37
Jacques Berger <jacquesberger@hotmail.com>
2012-04-22 21:39:28 -0400

    Renommer le package du projet

--- src/main/java/org/jberger/jsonparsingexample/json/JSON.java
+++ src/main/java/org/jberger/jsonparsingexample/json/JSON.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jberger.json;
+package org.jberger.jsonparsingexample.json;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
